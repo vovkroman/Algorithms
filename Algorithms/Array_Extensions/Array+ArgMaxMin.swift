cfd22075e5310b615c2ddb9a5900df1ff3541c1b
Roman Vovk <roman.vovk@globallogic.com>
2020-10-12 13:16:37 +0300

    Array: Arg max/min routines added implemented (in Swift)

---
+++ Algorithms/Array_Extensions/Array+ArgMaxMin.swift
@@ -0,0 +1,33 @@
+//
+//  Array+ArgMaxMin.swift
+//  Algorithms
+//
+//  Created by Roman Vovk on 12.10.2020.
+//  Copyright © 2020 Roman Vovk. All rights reserved.
+//
+
+import Foundation
+
+extension Array where Element: Comparable {
+    public func argmax() -> Index? {
+        return indices.max(by: { self[$0] < self[$1] })
+    }
+    
+    public func argmin() -> Index? {
+        return indices.min(by: { self[$0] < self[$1] })
+    }
+}
+
+extension Array {
+    public func argmax(by areInIncreasingOrder: (Element, Element) throws -> Bool) rethrows-> Index? {
+        return try indices.max { (i, j) throws -> Bool in
+            try areInIncreasingOrder(self[i], self[j])
+        }
+    }
+    
+    public func argmin(by areInIncreasingOrder: (Element, Element) throws -> Bool) rethrows-> Index? {
+        return try indices.min { (i, j) throws -> Bool in
+            try areInIncreasingOrder(self[i], self[j])
+        }
+    }
+}
