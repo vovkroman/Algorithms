a0f1cb4d92004c34eb6dbc13a9785f506aae25f9
Roman Vovk <roman.vovk@globallogic.com>
2020-10-15 11:55:33 +0300

    Queue: refactored replace Swift Array with ContiguousArray + renamed private property associated with _ (in Swift)

--- Algorithms/Queue/Swift/Queue.swift
+++ Algorithms/Queue/Swift/Queue.swift
@@ -9,29 +9,29 @@
 import Foundation
 
 public struct Queue<T> {
-  fileprivate var array = [T]()
-
-  public var count: Int {
-    return array.count
-  }
-
-  public var isEmpty: Bool {
-    return array.isEmpty
-  }
-
-  public mutating func enqueue(_ element: T) {
-    array.append(element)
-  }
+    private var _storage: ContiguousArray<T> = []
     
-  public mutating func dequeue() -> T? {
-    if isEmpty {
-      return nil
-    } else {
-      return array.removeFirst()
+    public var count: Int {
+        return _storage.count
     }
-  }
-
-  public var front: T? {
-    return array.first
-  }
+    
+    public var isEmpty: Bool {
+        return _storage.isEmpty
+    }
+    
+    public mutating func enqueue(_ element: T) {
+        _storage.append(element)
+    }
+    
+    public mutating func dequeue() -> T? {
+        if isEmpty {
+            return nil
+        } else {
+            return _storage.removeFirst()
+        }
+    }
+    
+    public var front: T? {
+        return _storage.first
+    }
 }
