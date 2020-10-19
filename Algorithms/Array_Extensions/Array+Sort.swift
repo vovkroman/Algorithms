3c07e31a276e051fe07aed57687926eee41a916c
Roman Vovk <roman.vovk@globallogic.com>
2020-10-19 19:52:18 +0300

    Array+Sort: updated selection sort (in Swift)

--- Algorithms/Array_Extensions/Array+Sort.swift
+++ Algorithms/Array_Extensions/Array+Sort.swift
@@ -1,23 +1,20 @@
 import Foundation
 
-extension Array {
-    public func selectionSort<T: Comparable>(_ array: [T], _ isOrderedBefore: (T, T) -> Bool) -> [T] {
-        guard array.count > 1 else { return array }
-        var a = array
-        for x in 0 ..< a.count - 1 {
-            // Find the lowest value in the rest of the array.
-            var lowest = x
-            for y in x + 1 ..< a.count {
-                if isOrderedBefore(a[y], a[lowest]) {
-                    lowest = y
-                }
-            }
-
-            // Swap the lowest value with the current array index.
-            if x != lowest {
-                a.swapAt(x, lowest)
+public func selectionSort<T: Comparable>(_ array: inout [T], _ isOrderedBefore: (T, T) -> Bool) -> [T] {
+    guard array.count > 1 else { return array }
+    var a = array
+    for x in 0 ..< a.count - 1 {
+        // Find the lowest value in the rest of the array.
+        var lowest = x
+        for y in x + 1 ..< a.count {
+            if isOrderedBefore(a[y], a[lowest]) {
+                lowest = y
             }
         }
-        return a
+        
+        if x != lowest {
+            a.swapAt(x, lowest)
+        }
     }
+    return a
 }
