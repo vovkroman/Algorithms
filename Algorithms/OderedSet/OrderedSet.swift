7e8d6f8239bdd5da8ebd72bd0ca82aa530112d21
Roman Vovk <roman.vovk@globallogic.com>
2020-12-07 20:15:34 +0200

    OrderedSet: added documentation comments

--- Algorithms/OderedSet/OrderedSet.swift
+++ Algorithms/OderedSet/OrderedSet.swift
@@ -1,58 +1,49 @@
 import Foundation
 /**
- * ***OrderedSet*** represents wrapper over Obj-C NSMutableOrderedSet.
- * *
- * NSMutableOrderedSet objects are not like C arrays. That is, even though you may specify a size when you create a mutable ordered set, the specified size is regarded as a “hint”; the actual size of the set is still 0. This means that you cannot insert an object at an index greater than the current count of an set. For example, if a set contains two objects, its size is 2, so you can add objects at indices 0, 1, or 2. Index 3 is illegal and out of bounds; if you try to add an object at index 3 (when the size of the array is 2), NSMutableOrderedSet raises an exception.
- *
- ** ***Application**:
- * - Search can be efficiently performed
+ **OrderedSet** represents wrapper over Obj-C NSMutableOrderedSet.
+ 
+ *NSMutableOrderedSet* objects are not like C arrays. That is, even though you may specify a size when you create a mutable ordered set, the specified size is regarded as a “hint”; the actual size of the set is still 0. This means that you cannot insert an object at an index greater than the current count of an set. For example, if a set contains two objects, its size is 2, so you can add objects at indices 0, 1, or 2. Index 3 is illegal and out of bounds; if you try to add an object at index 3 (when the size of the array is 2), NSMutableOrderedSet raises an exception.
+ 
+ **Application**:
+- Search can be efficiently performed
   
- * ***Performance**:
- * - building the Ordered array is O(n)
- * - inserting is O(n)
- * - searching item is O(log n)
- *
- * **Drawback**:
- * - Since using dynamic array under the hood, contains all drawbacks of the dynamic array
- **/
-
+ **Performance**:
+ - building the Ordered array is O(n)
+ - inserting is O(n)
+ - searching item is O(log n)
+ 
+ **Drawback**:
+ - Since using dynamic array under the hood, contains all drawbacks of the dynamic array*/
 public struct OrderedSet<T: Comparable & Hashable>: ExpressibleByArrayLiteral {
     @usableFromInline
     internal let _storage: NSMutableOrderedSet
     
     // MARK: - Initialization
     
-    /**
-     Description: *Initialization*
-        init as empty OrderedSet
-     */
+    /// Description: init as empty OrderedSet
     @inlinable
     public init() {
         _storage = NSMutableOrderedSet()
     }
-    /**
-     Description: *Initialization with capacity*
-     - parameter capacity: capacity of element, that need to be allocated under NSMutableOrderedSet
-     */
+    
+    /// Description: initialize empty set and
+    /// reserves enough space to store the specified number of elements.
+    /// - Parameter capacity: space of capacity
     @inlinable
     public init(capacity: Int) {
         _storage = NSMutableOrderedSet(capacity: capacity)
     }
     
-    /**
-     Description: *Initialization with any Container conforming Collection protocol*
-     - parameter collection: some container of items
-     */
+    /// Description: empty set from the collection
+    /// - Parameter collection: collection of items
     @inlinable
     public init<CollectionType: Collection>(collection: CollectionType) where CollectionType.Element == T {
         self.init(capacity: collection.count)
         insert(collection)
     }
     
-    /**
-     Description: *Creates an instance initialized with the given elements*
-     - parameter elements: given elements
-     */
+    /// Description: creates an instance initialized with the given elements
+    /// - Parameter elements: given elements
     @inlinable
     public init(arrayLiteral elements: T...) {
         self.init(collection: elements)
@@ -63,21 +54,15 @@ public struct OrderedSet<T: Comparable & Hashable>: ExpressibleByArrayLiteral {
         return _storage.count
     }
     
-    /**
-     Description: *Apply body closure to all objects*
-     
-     - parameter body: apply body closure to all element in collection as if run over loop for
-     */
+    /// Description: applies body closure to all objects
+    /// - Parameter body: apply body closure to all element in collection as if run over loop for
     @inlinable
     public func forEach(_ body: (T) -> Void) {
         _storage.forEach { body($0 as! T) }
     }
     
-    /**
-     Description: *Check if current element is contained in the NSMutableOrderedSet*
-     - parameter element: element of the NSMutableOrderedSet
-     - returns: true if current item is contained in the _storage, false otherwise
-     */
+    /// Description: checks up if current element is contained in the NSMutableOrderedSet
+    /// - Parameter element: element to be checked
     @inlinable
     public func contains(_ element: T) -> Bool {
         return _storage.contains(element)
@@ -93,9 +78,7 @@ public struct OrderedSet<T: Comparable & Hashable>: ExpressibleByArrayLiteral {
         }
     }
     
-    /**
-     Description: *Defines the order of NSMutableOrderedSet to be added*
-     */
+    /// Description: defines the order of NSMutableOrderedSet to be added
     @usableFromInline
     internal static func compare(_ a: Any, _ b: Any) -> ComparisonResult {
         let a = a as! T, b = b as! T
@@ -104,10 +87,8 @@ public struct OrderedSet<T: Comparable & Hashable>: ExpressibleByArrayLiteral {
         return .orderedSame
     }
     
-    /**
-     Description: *Insert item from any Collection
-     - parameter collection: any container. conformed Collection protocol (Set, Array)
-     */
+    /// Description: Insert item from any Collection
+    /// - Parameter seq: any container. conformed Collection protocol (Set, Array)
     @usableFromInline
     internal func insert<S: Sequence>(_ seq: S) where S.Element == T {
         for element in seq {
@@ -119,11 +100,9 @@ public struct OrderedSet<T: Comparable & Hashable>: ExpressibleByArrayLiteral {
 // MARK: - LOOKUP ELEMENT
 
 extension OrderedSet {
-    /**
-     Description: *Searching element by *element**
-     - parameter element
-     - returns: see **Result**
-     **/
+    
+    /// Description: searching element in set and returns *Result*
+    /// - Parameter element: element to be searched
     @inlinable
     public func lookUp(of element: T) -> Result<Int> {
         let index = _storage.index(of: element,
@@ -140,11 +119,9 @@ extension OrderedSet {
 // MARK: - INSERTION ELEMENT
 
 extension OrderedSet {
-    /**
-     Description: *Define index to be added to NSMutableOrderedSet*
-     - parameter value: element to be added
-     - returns: appropriate index of value to be added to keep ordered order
-     */
+    
+    /// Description: defines appropriate index to be added to NSMutableOrderedSet to keep ordered order
+    /// - Parameter value: element to be added
     @usableFromInline
     internal func index(for value: T) -> Int {
         return _storage.index(of: value,
@@ -153,12 +130,9 @@ extension OrderedSet {
                               usingComparator: OrderedSet.compare)
     }
     
-    /**
-       Description: find index of element, where element might be inserted and insert it, otherwise
-       ignore element if there is such element, does nothing
-       
-       - parameter newElement: new element of NSMutableOrderedSet
-       */
+    /// Description: find index of element, where element might be inserted and insert it,
+    /// otherwise ignore element if there is such element, does nothing
+    /// - Parameter newElement: element to insert
     @inlinable
     public func insert(_ newElement: T) {
         let index = self.index(for: newElement)
